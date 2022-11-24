e7c62f1892218d60c6c52c1d537bd030ecc76c50
ProgamEpuntoexe <81495787+ProgamEpuntoexe@users.noreply.github.com>
2022-11-23 19:35:00 -0600

    Commit de branch develop empty

--- programa/programaHospital/src/Main.java
+++ programa/programaHospital/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-
 //Clase doctor
 class doctor{
     public String nombre = "";
@@ -27,25 +26,7 @@ class paciente{
         citado = citadoLocal;
     }
 }
-//Clase cita
-class cita {
-    public paciente pacienteAtender;
-    public doctor doctorAtender;
-    public int horarioMinutos = 0;
-    public int horarioHoras = 0;
-    public int numeroMes = 0;
-    public int numeroDia = 0;
-    public int numeroYear = 0;
-    public cita(paciente pacienteLocal, doctor doctorLocal, int minutosLocal, int horasLocal, int mesLocal, int diaLocal, int yearlocal){
-        pacienteAtender = pacienteLocal;
-        doctorAtender = doctorLocal;
-        horarioHoras = horasLocal;
-        horarioMinutos = minutosLocal;
-        numeroMes = mesLocal;
-        numeroDia = diaLocal;
-        numeroYear = yearlocal;
-    }
-}
+
 //Clase recepcion
 class recepcion{
     public String nombreRecepcionista = "";
@@ -60,7 +41,7 @@ class edificio{
     public ArrayList<doctor> listaDoctores = new ArrayList<doctor>();
     public ArrayList<doctor> listaDoctoresDisponibles = new ArrayList<doctor>();
     public ArrayList<paciente> listaPacientes = new ArrayList<paciente>();
-    public ArrayList<cita> listaCitas = new ArrayList<cita>();
+    public ArrayList<Main.cita> listaCitas = new ArrayList<Main.cita>();
     String nombre = "";
     public edificio(String nombreLocal){
         nombre = nombreLocal;
@@ -73,61 +54,7 @@ class programa{
     public programa(){
 
     }
-    public void acceder(usuario datosUsuario) throws IOException{
-        boolean sesionActiva = false;
-        String nombreIntroducido = "";
-        String passwordIntroducido = "";
-        System.out.print("Ingrese el nombre de usuario: ");
-        nombreIntroducido = entrada.readLine();
-        System.out.print("Ingrese el su contraseña: ");
-        passwordIntroducido = entrada.readLine();
-        //Para introducir los datos
-        if (verificar(datosUsuario,nombreIntroducido,passwordIntroducido)){
-            //Verifica si el usuario esta autorizado o no
-            if (datosUsuario.autorizado == true){
-                System.out.println("Acceso al usuario consedido");
-                sesionActiva = true;
-            }else{
-                System.out.println("El usuario no esta autorizado");
-            }
-        }
-    }
-    //Dar de alta a los doctores/despedir
-    public void darAltaDoctor(edificio hospital, int idDoctor){
-        //Si hay doctores
-        if (!hospital.listaDoctores.isEmpty()){
-            hospital.listaDoctores.indexOf(idDoctor);
-        }else{
-            System.out.println("No hay doctores");
-        }
-    }
-    //Poner los datos del paciente y agendar una cita
-    public void agendarCita(edificio hospital, int idPaciente, int idDoctor){
-        //Despues de introducir los datos del paciente
-        //Si hay doctores
-        if (!hospital.listaDoctoresDisponibles.isEmpty()){
-            hospital.listaCitas.add(new cita(hospital.listaPacientes.get(idPaciente),hospital.listaDoctoresDisponibles.get(idDoctor),12,30,12,2,2023));
-        }else{
-            System.out.println("No hay Doctores disponibles");
-        }
-    }
-    //Dar de alta a un paciente si este esta sano
-    public void darAltaPaciente(edificio hospital, int idPaciente){
-        //Si hay pacientes
-        if (!hospital.listaPacientes.isEmpty()){
-            hospital.listaPacientes.remove(idPaciente);
-        }else{
-            System.out.println("No hay pacientes");
-        }
-    }
-    //Para verificar si los datos son correctos
-    private boolean verificar(usuario datosUsuario, String nombreIntroducido, String passwordIntroducido){
-        if ((datosUsuario.nombreUsuario == nombreIntroducido) & (datosUsuario.password == passwordIntroducido)){
-            return true;
-        }else{
-            return false;
-        }
-    }
+    
 }
 //Clase Usuario
 class usuario{
@@ -144,5 +71,25 @@ class usuario{
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static class cita {
+        public paciente pacienteAtender;
+        public doctor doctorAtender;
+        public int horarioMinutos = 0;
+        public int horarioHoras = 0;
+        public int numeroMes = 0;
+        public int numeroDia = 0;
+        public int numeroYear = 0;
+
+        public cita(paciente pacienteLocal, doctor doctorLocal, int minutosLocal, int horasLocal, int mesLocal, int diaLocal, int yearlocal) {
+            pacienteAtender = pacienteLocal;
+            doctorAtender = doctorLocal;
+            horarioHoras = horasLocal;
+            horarioMinutos = minutosLocal;
+            numeroMes = mesLocal;
+            numeroDia = diaLocal;
+            numeroYear = yearlocal;
+        }
     }
 }
